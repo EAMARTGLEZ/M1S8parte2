05654b750c8eb5d175c745c24926f957dfe5cc9a
EAMARTGLEZ <112790943+EAMARTGLEZ@users.noreply.github.com>
2023-02-09 21:49:17 -0600

    actulixacion de informacion
    se actulizaron los archivos index .html e  index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,38 @@
-console.log("Entro index.js");
+/*console.log("Entro index.js");
+
+let btnDanger = document.getElementById("btnDanger");
+
+function imprimirHolaMundo() {
+    alert("Hola Mundo");
+}
+btnDanger.addEventListener("click",() =>{
+    alert("Hola mundo desde boton danger");
+})*/
+document.addEventListener('DOMContentLoaded', () =>{
+
+    const saveBtn = document.getElementById("saveBtn"); 
+    const ChangeThemeBtn = document.getElementById("ChangeThemeBtn"); 
+    const inputName = document.getElementById("inputName"); 
+    const inputPuesto = document.getElementById("inputPuesto"); 
+    const tableBody = document.getElementById("tableBody"); 
+    function loadData(){
+        console.log("Entro a Load Data");
+    }
+    saveBtn.addEventListener('click', () =>{
+        const name = inputName.value;
+        const puesto = inputPuesto.value;
+        if (name === "") {
+            console.log("ingresa tu nombre por favor");
+        }else{
+            console.log(name);
+        }
+        console.log(puesto);
+        let empleado = JSON.parse(localStorage.getItem("empleado")) || [];
+        empleado.push({
+            name,
+            puesto
+        });
+        localStorage.setItem(empleado, JSON.stringify(empleado));
+    });
+    loadData();
+});
